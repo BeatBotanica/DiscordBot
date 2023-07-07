46d3c1ae5177d199c5d8d8a687ce8d31aaad9bd5
kusaymoo <plahsoekusay@gmail.com>
2023-07-07 18:16:22 +1200

    add intents and test message create.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-require('dotenv').config(); //initialize dotenv
-const Discord = require('discord.js'); //import discord.js
+require("dotenv").config(); //initialize dotenv
+const { Client, GatewayIntentBits } = require("discord.js"); //import discord.js
 
-const client = new Discord.Client(); //create new client
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+}); //create new client
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
+  }
 });
 
 //make sure this line is the last line
