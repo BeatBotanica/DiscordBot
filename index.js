7719e6cc49fb0bfd635dba7033cba01b3ed6d45f
Gareth McIntosh <mcintogare@le-metropolitain.ecs.vuw.ac.nz>
2023-07-16 16:44:55 +1200

    can now react to select a song to display samples for

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-require("dotenv").config(); //initialize dotenv
-const axios = require("axios"); //import axios
-const { Client, GatewayIntentBits } = require("discord.js"); //import discord.js
+require("dotenv").config(); // Initialize dotenv
+const axios = require("axios"); // Import axios
+const { Client, GatewayIntentBits } = require("discord.js"); // Import discord.js
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
   ],
-}); //create new client
+}); // Create new client
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -17,6 +18,8 @@ client.on("ready", () => {
 const config = {
   prefix: "!",
 };
+
+let results;
 
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
@@ -30,20 +33,26 @@ client.on("messageCreate", async (message) => {
     case "samplify":
       try {
         let query = args.join(" ");
-        const results = await getSearchResults(query);
+        results = await getSearchResults(query);
         // for each result, send its title and artist
 
         let text = "Showing results for " + query + "\n";
 
         results.forEach((result, index) => {
-          text += index+1 + ": " + result.title + " by " + result.artist + "\n";
+          text += index + 1 + ": " + result.title + " by " + result.artist + "\n";
         });
 
-        message.reply(text);
+        const reply = message.reply(text);
+        reply.then((msg) => {
+          msg.react("1️⃣");
+          msg.react("2️⃣");
+          msg.react("3️⃣");
+          msg.react("4️⃣");
+        });
       } catch (err) {
         message.reply(
           "Something went wrong. Please make sure to use the following format if you haven't: !samplify <search query> \n" +
-            err.toString()
+          err.toString()
         );
       }
       break;
@@ -55,19 +64,59 @@ client.on("messageCreate", async (message) => {
 client.on("messageReactionAdd", async (reaction, user) => {
   if (user.bot) return;
 
-  if (reaction.emoji.name === "1️⃣") {
-    const message = reaction.message;
-    message.reply("You reacted with a 1");
+  let reactionIndex = 0;
 
-  } else if(reaction.emoji.name === "2️⃣") {
+  if (reaction.message.content.startsWith("Showing results for ")) {
+    if (reaction.emoji.name === "1️⃣") {
+      reactionIndex = 0;
+    } else if (reaction.emoji.name === "2️⃣") {
+      reactionIndex = 1;
+    } else if (reaction.emoji.name === "3️⃣") {
+      reactionIndex = 2;
+    } else if (reaction.emoji.name === "4️⃣") {
+      reactionIndex = 3;
+    }
 
+    const result = results[reactionIndex];
+    const samples = await getSamples(result.id);
+    const reply = await reaction.message.reply("Loading samples...");
 
-  } else if(reaction.emoji.name === "3️⃣") {
+    // Prepare the formatted message content and embeds
+    const formattedSamples = JSON.parse(samples);
+    if(formattedSamples.length === 0) {
+      reply.edit("No samples found.");
+      return;
+    }
+    const embeds = [];
 
+    formattedSamples.forEach((sample, index) => {
+      const sampleContent =
+        `  Title: ${sample.title}\n` +
+        `  Artist: ${sample.artist}\n` +
+        `  Year: ${sample.year}\n`;
 
-  } else if(reaction.emoji.name === "4️⃣") {
+      const embed = {
+        description: sampleContent,
+        image: {
+          url: sample.imgUrl,
+        },
+      };
 
+      embeds.push(embed);
+    });
 
+    // Edit the loading message with the formatted samples content and images
+    embeds.forEach((embed, index) => {
+      if (index === 0) {
+        reply.edit({
+          embeds: [embed],
+        });
+      } else {
+        reply.channel.send({
+          embeds: [embed],
+        });
+      }
+    });
   }
 });
 
@@ -75,7 +124,7 @@ async function getSamples(id) {
   const res = await axios.get(
     `https://rangi.beatbotanica.com/api/samples?id=${id}`
   );
-  return JSON.stringify(res.data);
+   return JSON.stringify(res.data);
 }
 
 async function getSearchResults(query) {
@@ -85,5 +134,5 @@ async function getSearchResults(query) {
   return res.data;
 }
 
-//make sure this line is the last line
-client.login(process.env.CLIENT_TOKEN); //login bot using token
+// Make sure this line is the last line
+client.login(process.env.CLIENT_TOKEN); // Login bot using token
